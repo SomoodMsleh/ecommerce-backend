9cd492499609c21094e8f03a78f13f1ef5d24d50
SomoodMsleh <somoodmedwan@gmail.com>
2025-12-20 16:01:33 +0200

    database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,9 +3,25 @@ import logger  from '../utils/logger.util.js';
 
 const connectDB = async (): Promise<void> => {
     try{
+        const options = {
+            maxPoolSize : 10,  // Maintain up to 10 socket connections
+            serverSelectionTimeoutMS: 5000, // keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // close sockets after 45 seconds of inactivity
+        }
+        await mongoose.connect(process.env.DB_URL!, options); // (!) to assert that DB_URL is not undefined 
+        logger.info('✅ MongoDB connected successfully');
+
+        mongoose.connection.on('error', (err)=>{
+            logger.error('❌ MongoDB connection error: ', err);
+        }); // listen for errors after initial connection
+
+        mongoose.connection.on('disconnected' , ()=>{
+            logger.warn(' MongoDB disconnected! trying to reconnect...')
+        }); // listen for disconnection events
 
     }catch(error){
-
+        logger.error('❌ mongoDB connection error: ', error);
+        process.exit(1); // exit process with failure
     }
 };
 
--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -2,17 +2,17 @@ import winston from 'winston';
 import {fileURLToPath} from 'url';
 import { dirname , join } from  'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // get current file path
+const __dirname = dirname(__filename); // get current directory path
 
 const logger = winston.createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug', // set log level based on environment 
     format: winston.format.combine(
-        winston.format.timestamp({format:'YYYY-MMDD HH:mm:ss'}),
-        winston.format.errors({stack : true}),
-        winston.format.splat(),
+        winston.format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}), 
+        winston.format.errors({stack : true}), // log error stack trace
+        winston.format.splat(), // string interpolation
         winston.format.json()
-    ),
+    ), // log format
     transports:[
         new winston.transports.File({
             filename: join(__dirname, '../../logs/error.log'),
@@ -20,9 +20,10 @@ const logger = winston.createLogger({
         }),
         new winston.transports.File({
             filename: join(__dirname, '../../logs/combined.log')
-        })
-    ]
-});
+        }) // all logs
+    ] // log to files
+}); // logger configuration
+
 
 if (process.env.NODE_ENV !== 'production'){
     logger.add(new winston.transports.Console({
@@ -30,7 +31,7 @@ if (process.env.NODE_ENV !== 'production'){
             winston.format.colorize(),
             winston.format.simple()
         )
-    }))
+    })) // log to console in non-production
 }
 
 
