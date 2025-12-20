ef14780a766bbb1250f109c1e7c456069269a4cb
SomoodMsleh <somoodmedwan@gmail.com>
2025-12-20 20:41:50 +0200

    error middleware to Global Error Handling Middleware

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import {} from './routes/index.routes.js';
-import {} from './middlewares/error.middleware.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app:Application = express(); 
 app.use(helmet()); 
@@ -11,4 +11,5 @@ app.use(express.json({limit: '10mb'})); // parse JSON request bodies with a limi
 app.use(express.urlencoded({ extended:true, limit: '10mb'})); // parse URL-encoded request bodies with a limit of 10mb
 
 
+app.use(errorMiddleware); // Global Error Handling Middleware
 export default app;
---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,20 @@
+import ApiError from "../utils/error.util.js";
+import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger.util.js";
+
+const errorMiddleware = async(err: Error, req: Request, res: Response, next: NextFunction): Promise<void> =>{
+    if (err instanceof ApiError){
+        res.status(err.statusCode).json({
+            success: false,
+            message : err.message
+        }); 
+        return;
+    } // Handling custom ApiError
+    logger.error('Unhandled Error:', err); 
+    res.status(500).json({
+        success:false,
+        message : 'Internal Server Error'
+    }); // Handling generic errors
+}; //Global Error Handling Middleware
+
+export default errorMiddleware;
