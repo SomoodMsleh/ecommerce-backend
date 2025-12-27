64dc9072b9e83672548e2b0e2d99276e490c4b6c
SomoodMsleh <somoodmedwan@gmail.com>
2025-12-27 16:47:29 +0200

    uploads file middelware using multer

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,3 +1,25 @@
 import multer from "multer";
 import ApiError from "../utils/error.util.js";
+import { Request } from "express";
 
+
+export const fileValidation ={
+    image:['image/png','image/jpeg','image/jpg','image/webp'],
+    pdf:['application/pdf'],
+    excel:['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+} 
+
+const fileUpload = (allowedTypes: string[] = [])=>{
+    const storage = multer.memoryStorage();
+    function fileFilter(req:Request,file:Express.Multer.File,cb:multer.FileFilterCallback){
+        if (allowedTypes.includes(file.mimetype)){
+            cb(null,true);
+        }else{
+            cb(new ApiError('Invalid file format', 400))
+        }
+    }
+    const upload = multer({fileFilter,storage,limits:{fileSize:5*1024*1024}});
+    return upload;
+}
+
+export default fileUpload;
---
+++ src/utils/cloudinary.util.ts
@@ -0,0 +1,3 @@
+import cloudinary from "../config/cloudinary.config.js";
+
+
