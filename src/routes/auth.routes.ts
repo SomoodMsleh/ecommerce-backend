a79bb4e18333c1faa1208f8e56d7b3c073a9f138
SomoodMsleh <somoodmedwan@gmail.com>
2025-12-27 13:08:11 +0200

    add ratelimiter to routes and some eidt

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -8,26 +8,26 @@ import passport from "passport";
 import ratelimiter from '../middlewares/rateLimiter.middleware.js';
 const router = Router();
 
-router.post('/',validation(authValidation.registerSchema),asyncHandler(authController.register));
+router.post('/',ratelimiter({windowMs:15*60*1000,max:5}),validation(authValidation.registerSchema),asyncHandler(authController.register));
 router.post('/verifyEmail',validation(authValidation.verifyEmailSchema),asyncHandler(authController.isEmailVerified));
-router.post('/login',validation(authValidation.loginSchema),asyncHandler(authController.login));
-router.post('/logout',asyncHandler(authController.logout));
+router.post('/login',ratelimiter({windowMs:15*60*1000,max:10}),validation(authValidation.loginSchema),asyncHandler(authController.login));
+router.post('/logout',authenticate,asyncHandler(authController.logout));
 
-router.post('/forgetPassword',validation(authValidation.forgotPasswordSchema),asyncHandler(authController.forgetPassword));
-router.post('/resetPassword/:token',validation(authValidation.resetPasswordSchema),asyncHandler(authController.resetPassword));
+router.post('/forgetPassword',ratelimiter({windowMs:15*60*1000,max:5}),validation(authValidation.forgotPasswordSchema),asyncHandler(authController.forgetPassword));
+router.post('/resetPassword/:token',ratelimiter({windowMs:15*60*1000,max:5}),validation(authValidation.resetPasswordSchema),asyncHandler(authController.resetPassword));
 
 
 router.post('/refresh',asyncHandler(authController.refreshToken));
 
 router.post('/2fa/enable',authenticate,asyncHandler(authController.enable2FA));
 router.post('/2fa/verify',authenticate,validation(authValidation.verify2FASchema),asyncHandler(authController.verify2FA));
-router.post('/2fa/verify-login',validation(authValidation.verify2FALoginSchema),asyncHandler(authController.verify2FALogin));
+router.post('/2fa/verify-login',ratelimiter({windowMs:15*60*1000,max:5}),validation(authValidation.verify2FALoginSchema),asyncHandler(authController.verify2FALogin));
 router.post('/2fa/disable',authenticate,validation(authValidation.disable2FASchema),asyncHandler(authController.disable2FA));
 
-router.get('/google/',passport.authenticate('google',{scope:['profile','email'],session:false}));
+router.get('/google/',ratelimiter({windowMs:15*60*1000,max:5}),passport.authenticate('google',{scope:['profile','email'],session:false}));
 router.get('/google/callback',passport.authenticate('google',{failureMessage:true,session:false}),asyncHandler(authController.googleCallback));
 
-router.get('/facebook/',passport.authenticate('facebook',{scope:['profile','email'],session:false}));
+router.get('/facebook/',ratelimiter({windowMs:15*60*1000,max:5}),passport.authenticate('facebook',{scope:['email'],session:false}));
 router.get('/facebook/callback',passport.authenticate('facebook',{failureMessage:true,session:false}),asyncHandler(authController.facebookCallback));
 
 
