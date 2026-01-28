9e3be050a6481e1459b21a2cfff8a566cd257f6d
SomoodMsleh <somoodmedwan@gmail.com>
2026-01-28 23:50:48 +0200

    setup paypal payment using '@paypal/paypal-server-sdk'

--- src/config/payment.config.ts
+++ src/config/payment.config.ts
@@ -1,10 +1,30 @@
 import Stripe from 'stripe';
-import {PayPalServerSDKClient } from '@paypal/paypal-server-sdk';
+import { Client, Environment, LogLevel, OrdersController} from '@paypal/paypal-server-sdk'
 
 // Stripe Configuration
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
     apiVersion: "2025-02-24.acacia" as const,
 });
 
+// paypal Configuration
+const environment = process.env.NODE_ENV === 'production' ? Environment.Production : Environment.Sandbox;
+const logging = {
+    logLevel : process.env.NODE_ENV === 'production' ? LogLevel.Error: LogLevel.Info,
+    logRequest:{
+        logBody:process.env.NODE_ENV !== 'production' ,
+    }, 
+    logResponse:{
+        logBody:process.env.NODE_ENV !== 'production' ,
+    }
+};
 
+export const paypalClient = new Client({
+    clientCredentialsAuthCredentials:{
+        oAuthClientId: process.env.PAYPAL_CLIENT_ID!,
+        oAuthClientSecret: process.env.PAYPAL_CLIENT_SECRET!,
+    },
+    environment,
+    logging,
+});
 
+export const paypalOrdersController = new OrdersController(paypalClient);
